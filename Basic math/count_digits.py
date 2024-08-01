aa8546f5bf89c6aafdbfea0c80c2f545ea0f609f
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-01 23:43:56 +0530

    Create count_digits.py

---
+++ Basic math/count_digits.py
@@ -0,0 +1,12 @@
+# link  - https://www.geeksforgeeks.org/problems/count-digits5716/1
+def evenlyDivides (self, N):
+        # code here
+        count = 0 
+        original_num = N
+        while(N>0):
+            last_digit = N%10
+            N = N//10
+            if original_num %last_digit ==0 and last_digit!=0:
+                count+=1
+            
+        return count
