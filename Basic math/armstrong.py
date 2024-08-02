f472dd6fd1a63e2cf4006ddc46b845cde25cee5b
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-02 23:39:05 +0530

    Update armstrong.py

--- Basic math/armstrong.py
+++ Basic math/armstrong.py
@@ -2,11 +2,14 @@
 def checkArmstrong(n):
     #write your code here !!!!!!!!!
     sum = 0
+    length_of_n = len(str(n))
     original = n
     while(n>0):
         last_digit = n%10
         n = n//10
-        sum = sum+(last_digit*last_digit*last_digit)
+        sum = sum+(last_digit**length_of_n)
     return original == sum
+
+    
 
     
