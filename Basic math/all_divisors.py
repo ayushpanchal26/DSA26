abf887b5b1fab06b956dd3a3b7aa4716ec396133
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-05 23:13:34 +0530

    Update all_divisors.py

--- Basic math/all_divisors.py
+++ Basic math/all_divisors.py
@@ -8,5 +8,18 @@ def printDivisors(n: int) -> List[int]:
         if (n%i==0):
             divisors.append(i)
     return divisors
-    
-    
+
+
+# better code with better runtime
+from typing import List
+import math
+
+def printDivisors(n: int) -> List[int]:
+    divisors = []
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n // i)
+    divisors.sort()  
+    return divisors
