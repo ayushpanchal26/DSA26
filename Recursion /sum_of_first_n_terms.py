1e437ad9815f3f9280a2d5e859ddc1d4f5357215
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-19 23:50:14 +0530

    Update sum_of_first_n_terms.py

--- Recursion /sum_of_first_n_terms.py
+++ Recursion /sum_of_first_n_terms.py
@@ -5,3 +5,11 @@ def sumOfSeries(self,n):
     return 1
   return n**3+self.sumOfSeries(n-1)
   
+# optimal way to do these question is 
+def sumOfSeries(self,n):
+        #code here
+        sum_n = n*(n+1)//2
+        return sum_n**2
+  # because in recursion method the time limit will be exceeded.
+  
+
