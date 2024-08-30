00a1fdf5b6216a775319efa0b1572578e91d2bb8
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-30 21:07:10 +0530

    Create selection_sort.cpp

---
+++ sorting/cpp/selection_sort.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void selection(int arr[], int n){
+    for(int i = 0  ; i<=n-2;i++){
+        int mini = i;
+        for (int j = i+1 ;j<=n-1;j++){
+            if (arr[j]< arr[mini]){
+                mini = j;
+                
+            }
+        // swap
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i]= temp;
+        }
+    }
+    for (int i = 0;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main() {
+    // Write C++ code here
+    int arr[] = {13,46,24,52,20,9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for (int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    selection(arr,n);
+    return 0;
+}
