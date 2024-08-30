a240f1631c60393de6e3a39cbdd87e2ea436a122
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-08-30 23:11:44 +0530

    Update selection_sort.cpp

--- sorting/cpp/selection_sort.cpp
+++ sorting/cpp/selection_sort.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 void selection(int arr[], int n){
-    for(int i = 0  ; i<=n-1;i++){
+    for(int i = 0  ; i<n-1;i++){
         int mini = i;
-        for (int j = i+1 ;j<=n;j++){
+        for (int j = i+1 ;j<n;j++){
             if (arr[j]< arr[mini]){
                 mini = j;
                 
