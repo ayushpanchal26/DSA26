41e7f4bf10fd8d927bcbdbe02c76e723921aa180
ayushpanchal26 <ayushpanchal2605@gmail.com>
2024-08-30 23:32:44 +0530

    geek_for_geeks

---
+++ Leetcode or any platform/selection_sort.py
@@ -0,0 +1,16 @@
+# link - https://bit.ly/3ppA6YJ
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        pass
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(0,n-1):
+            min_index = i
+            for j in range(i,n):
+                if arr[j]<arr[min_index]:
+                    min_index = j
+            arr[min_index],arr[i] = arr[i],arr[min_index]
