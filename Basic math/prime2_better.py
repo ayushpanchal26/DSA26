f5affe34e320bc8273156c3a853bc4d6817c57b5
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2026-01-29 22:57:14 +0530

    Create prime2_better.py

---
+++ Basic math/prime2_better.py
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int count = 0;
+	int n;
+	cin>>n;
+	
+	for(int i=1;i*i<=n;i++){
+		if(n%i==0){
+			count++;
+			if ((n/i)!=i){
+				count++;
+			}
+		}
+	}
+	if (count==2) cout<<"true";
+	else cout<<"false";
+}
